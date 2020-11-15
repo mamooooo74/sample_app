4cc71200d094ce74e77a186bbfcac3c7b6a23deb
mamoru <mamooo.74@icloud.com>
2020-11-15 11:23:50 +0000

    FINISH CH12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,15 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
 
-    mail      to: user.email, 
+    mail           to: user.email, 
               subject: "Account activation"
   end
 
   
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail           to: user.email, 
+              subject: "Password reset"
   end
 end
